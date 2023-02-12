6c350fbfba7c02fb667440e5e12f420d0311897b
Bobby-PC <Bobby-PC@BOBBY-PC>
2023-02-12 12:20:39 +0200

    Add project files.

---
+++ 4.ListOfProducts/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _4.ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> product = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                product.Add(currentProduct);
+            }
+
+            product.Sort();
+            for (int i = 0; i < product.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{product[i]}");
+            }
+        }
+    }
+}
---
+++ 5.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _5.RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = new List<int>();
+
+            foreach (var item in numbers) 
+            {
+                if (item > 0)
+                {
+                    result.Add(item);
+                }
+            }
+            if (result.Count > 0) 
+            {
+                result.Reverse();
+                Console.WriteLine(string.Join(" ", result));
+               
+            }
+            else 
+            {
+                Console.WriteLine("empty");
+            }
+
+        }
+    }
+}
